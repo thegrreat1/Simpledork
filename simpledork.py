921cc0c6e2152c4e70906e80794a5562543de4a0
Haxmax <63957530+thegrreat1@users.noreply.github.com>
2023-07-06 17:29:24 -0400

    Add files via upload

--- simpledork.py
+++ simpledork.py
@@ -2,6 +2,7 @@ import sys
 import random
 import time
 from fake_useragent import UserAgent
+import subprocess
 
 try:
     from googlesearch import search
@@ -21,18 +22,20 @@ print("""
 
 ua = UserAgent()
 
- 
 if len(sys.argv) >= 2:
 	if __name__ == "__main__":
 		query = sys.argv[1]
 		pages = int(sys.argv[2])   
 		
-		print("To prevent google captcha this is going to take a while. Go grab a coffee! ;)\n")
-	for j in search(query, tld="co.in", num=pages, stop=pages, pause=60 + random.randrange(2, 20), user_agent=ua.random):
-		print(j)
-		time.sleep(random.uniform(1, 3))
+		print("\x1b[0;30;41mTo prevent google captcha this is going to take a while. Go grab a coffee! ;)\x1b[0m\n")
+	try:
+		for j in search(query, tld="co.in", num=pages, stop=pages, pause=150 + random.randrange(10, 100), user_agent=ua.random):
+			print(j)
+	except Exception as e:
+		print(f"An error occurred during the search: {e}")
 else:
 	print("Usage: python3 simpledork.py QUERY AMOUNTOFPAGESTOSEARCH")
 	print("Example: python3 simpledork.py inurl:" + '"' + "/admin/login.php" +'"' + " 10")
-	sys.exit(1)
+sys.exit(1)
+
 	
